e239f74c90a3dd9c1ea9506442ad36962ec29d92
fatihklc817 <mf.kilic@hotmail.com>
2022-09-23 20:06:15 +0300

    Game Scene Added

---
+++ Assets/Game/Scripts/PickerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickerController : MonoBehaviour
+{
+    [SerializeField] private float _forwardMoveSpeed;
+    [SerializeField] private float _horizontalMoveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+
+        transform.position = transform.position + Vector3.forward * Time.deltaTime * _forwardMoveSpeed;
+        
+
+
+        if (Input.GetMouseButton(0))
+        {
+            
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit,100f))
+            { 
+                 Debug.DrawRay(ray.origin,ray.direction*100f, Color.white);
+                if (hit.transform.CompareTag("floor"))
+                {
+                    Vector3 hitVector = hit.point;
+                    Vector3 targetPos = new Vector3(hitVector.x, transform.position.y, transform.position.z);
+
+                    transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * _horizontalMoveSpeed);
+                }
+            }
+        }
+        
+    }
+}
---
+++ Assets/Game/Scripts/followCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _followingObject;
+
+    private Camera _camera;
+    private Vector3 _cameraOffset;
+     private float _cameraLerpFloat = 0.5f;
+   
+
+    void Start()
+    {
+        _camera = Camera.main;
+        _cameraOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+
+        Vector3 targetPos = new Vector3 (transform.position.x,transform.position.y,_followingObject.position.z)+ _cameraOffset;
+        Vector3 SmoothTargetPos = Vector3.Lerp(transform.position, targetPos, _cameraLerpFloat) ;
+        transform.position = SmoothTargetPos;
+        
+    }
+}
